96859ce2fc768b17b96f73cae1e50fc96bd035d2
Jiheon Kim <siwmua0@naver.com>
2024-11-02 21:56:14 +0900

    개발환경 마무리, electron-builder 설정 추가, 실행 스크립트 최적화

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,20 +1,16 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
 
-/* 
-app
-node.js랑 마찬가지로 이벤트기반의 api
-일렉트론 어플리케이션의 생명주기를 관리
-
-*/
+const isDev = !app.isPackaged;
 
 function createWindow() {
-  const mainWindow = new BrowserWindow({
-    // 굉장히 많은 옵션들
-  });
-  mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
-  //  C:\user\desktop\resouce-manager
-  //  /dist-react/index.html
+  const mainWindow = new BrowserWindow({});
+
+  if (isDev) {
+    mainWindow.loadURL("http://localhost:5123");
+  } else {
+    mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+  }
 }
 
 app.whenReady().then(createWindow);
